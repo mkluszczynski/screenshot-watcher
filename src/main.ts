df01c277e689aad6395da8f221fb50648b84f80c
mkluszczynski <mkluszczynski07@gmail.com>
2025-02-25 10:48:33 +0100

    feat: enhance build scripts with minification and sourcemaps; comment out notifier usage

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { watch } from "fs";
 import { Config } from "./config";
 import { uploadScreenShot } from "./upload";
 import clipboard from "clipboardy";
-import notifier from "node-notifier";
 
 const config = Config.load();
 
@@ -12,7 +11,7 @@ const watcher = watch(config.localPath, (event, filename) => {
 
   uploadScreenShot(filename);
   clipboard.writeSync(config.url + filename);
-  notifier.notify({title: "Screenshot watcher", message: `URL copied to clipboard: ${config.url + filename}`});
+  // notifier.notify({title: "Screenshot watcher", message: `URL copied to clipboard: ${config.url + filename}`});
   
 });
 
--- src/upload.ts
+++ src/upload.ts
@@ -1,6 +1,5 @@
 import Client from "ssh2-sftp-client";
 import { Config } from "./config";
-import notifier from 'node-notifier';
 
 const config = Config.load();
 
@@ -21,7 +20,7 @@ export function uploadScreenShot(fileName: string) {
     })
     .catch((err) => {
         console.log("[!!] Upload failed, please check your credentials and try again.", err.message);
-        notifier.notify({title: "Screenshot watcher", message: `Upload failed, please check your credentials and try again.`});
+        // notifier.notify({title: "Screenshot watcher", message: `Upload failed, please check your credentials and try again.`});
         process.exit()
     });
 
